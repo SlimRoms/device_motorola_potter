2168dffd6f3ef90b145056d00f23a1e35d249b85
Vachounet <vachounet@live.fr>
2017-05-06 18:10:22 +0200

    potter: slim bringup
    Change-Id: I95551b794cdb05e854e6e6c186bd46f7dd2d7fb2

--- slim.mk
+++ slim.mk
@@ -23,7 +23,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, vendor/motorola/potter/potter-vendor.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/slim/config/common_full_phone.mk)
 
 # Boot animation
 TARGET_SCREEN_WIDTH := 1080
@@ -31,7 +31,7 @@ TARGET_SCREEN_HEIGHT := 1920
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := potter
-PRODUCT_NAME := lineage_potter
+PRODUCT_NAME := slim_potter
 PRODUCT_BRAND := motorola
 PRODUCT_MANUFACTURER := motorola
 
